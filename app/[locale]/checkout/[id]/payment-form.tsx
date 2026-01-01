ee22a0dd80f35a50d96ba5817e32ef0ad13914be
walid shehzad <shehzadwalid@gmail.com>
2026-01-01 11:22:13 +0500

    COD-added

--- app/[locale]/checkout/[id]/payment-form.tsx
+++ app/[locale]/checkout/[id]/payment-form.tsx
@@ -1,41 +1,31 @@
-'use client'
+"use client";
 
-import {
-  PayPalButtons,
-  PayPalScriptProvider,
-  usePayPalScriptReducer,
-} from '@paypal/react-paypal-js'
-import { Card, CardContent } from '@/components/ui/card'
-import { useToast } from '@/hooks/use-toast'
-import {
-  approvePayPalOrder,
-  createPayPalOrder,
-} from '@/lib/actions/order.actions'
-import { IOrder } from '@/lib/db/models/order.model'
-import { formatDateTime } from '@/lib/utils'
+import { Card, CardContent } from "@/components/ui/card";
+import { useToast } from "@/hooks/use-toast";
+import { IOrder } from "@/lib/db/models/order.model";
+import { formatDateTime } from "@/lib/utils";
 
-import CheckoutFooter from '../checkout-footer'
-import { redirect, useRouter } from 'next/navigation'
-import { Button } from '@/components/ui/button'
-import ProductPrice from '@/components/shared/product/product-price'
-import StripeForm from './stripe-form'
-import { Elements } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
+import CheckoutFooter from "../checkout-footer";
+import { redirect, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import ProductPrice from "@/components/shared/product/product-price";
+import StripeForm from "./stripe-form";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
-)
+);
+
 export default function OrderDetailsForm({
   order,
-  paypalClientId,
   clientSecret,
 }: {
-  order: IOrder
-  paypalClientId: string
-  isAdmin: boolean
-  clientSecret: string | null
+  order: IOrder;
+  isAdmin: boolean;
+  clientSecret: string | null;
 }) {
-  const router = useRouter()
+  const router = useRouter();
   const {
     shippingAddress,
     items,
@@ -46,94 +36,57 @@ export default function OrderDetailsForm({
     paymentMethod,
     expectedDeliveryDate,
     isPaid,
-  } = order
-  const { toast } = useToast()
+  } = order;
+  const { toast } = useToast();
 
   if (isPaid) {
-    redirect(`/account/orders/${order._id}`)
-  }
-  function PrintLoadingState() {
-    const [{ isPending, isRejected }] = usePayPalScriptReducer()
-    let status = ''
-    if (isPending) {
-      status = 'Loading PayPal...'
-    } else if (isRejected) {
-      status = 'Error in loading PayPal.'
-    }
-    return status
-  }
-  const handleCreatePayPalOrder = async () => {
-    const res = await createPayPalOrder(order._id)
-    if (!res.success)
-      return toast({
-        description: res.message,
-        variant: 'destructive',
-      })
-    return res.data
-  }
-  const handleApprovePayPalOrder = async (data: { orderID: string }) => {
-    const res = await approvePayPalOrder(order._id, data)
-    toast({
-      description: res.message,
-      variant: res.success ? 'default' : 'destructive',
-    })
+    redirect(`/account/orders/${order._id}`);
   }
 
   const CheckoutSummary = () => (
     <Card>
-      <CardContent className='p-4'>
+      <CardContent className="p-4">
         <div>
-          <div className='text-lg font-bold'>Order Summary</div>
-          <div className='space-y-2'>
-            <div className='flex justify-between'>
+          <div className="text-lg font-bold">Order Summary</div>
+          <div className="space-y-2">
+            <div className="flex justify-between">
               <span>Items:</span>
               <span>
-                {' '}
+                {" "}
                 <ProductPrice price={itemsPrice} plain />
               </span>
             </div>
-            <div className='flex justify-between'>
+            <div className="flex justify-between">
               <span>Shipping & Handling:</span>
               <span>
                 {shippingPrice === undefined ? (
-                  '--'
+                  "--"
                 ) : shippingPrice === 0 ? (
-                  'FREE'
+                  "FREE"
                 ) : (
                   <ProductPrice price={shippingPrice} plain />
                 )}
               </span>
             </div>
-            <div className='flex justify-between'>
+            <div className="flex justify-between">
               <span> Tax:</span>
               <span>
                 {taxPrice === undefined ? (
-                  '--'
+                  "--"
                 ) : (
                   <ProductPrice price={taxPrice} plain />
                 )}
               </span>
             </div>
-            <div className='flex justify-between  pt-1 font-bold text-lg'>
+            <div className="flex justify-between  pt-1 font-bold text-lg">
               <span> Order Total:</span>
               <span>
-                {' '}
+                {" "}
                 <ProductPrice price={totalPrice} plain />
               </span>
             </div>
 
-            {!isPaid && paymentMethod === 'PayPal' && (
-              <div>
-                <PayPalScriptProvider options={{ clientId: paypalClientId }}>
-                  <PrintLoadingState />
-                  <PayPalButtons
-                    createOrder={handleCreatePayPalOrder}
-                    onApprove={handleApprovePayPalOrder}
-                  />
-                </PayPalScriptProvider>
-              </div>
-            )}
-            {!isPaid && paymentMethod === 'Stripe' && clientSecret && (
+            {!isPaid && paymentMethod === "Stripe" && clientSecret && (
               <Elements
                 options={{
                   clientSecret,
@@ -147,9 +100,9 @@ export default function OrderDetailsForm({
               </Elements>
             )}
 
-            {!isPaid && paymentMethod === 'Cash On Delivery' && (
+            {!isPaid && paymentMethod === "Cash On Delivery" && (
               <Button
-                className='w-full rounded-full'
+                className="w-full rounded-full"
                 onClick={() => router.push(`/account/orders/${order._id}`)}
               >
                 View Order
@@ -159,19 +112,19 @@ export default function OrderDetailsForm({
         </div>
       </CardContent>
     </Card>
-  )
+  );
 
   return (
-    <main className='max-w-6xl mx-auto'>
-      <div className='grid md:grid-cols-4 gap-6'>
-        <div className='md:col-span-3'>
+    <main className="max-w-6xl mx-auto">
+      <div className="grid md:grid-cols-4 gap-6">
+        <div className="md:col-span-3">
           {/* Shipping Address */}
           <div>
-            <div className='grid md:grid-cols-3 my-3 pb-3'>
-              <div className='text-lg font-bold'>
+            <div className="grid md:grid-cols-3 my-3 pb-3">
+              <div className="text-lg font-bold">
                 <span>Shipping Address</span>
               </div>
-              <div className='col-span-2'>
+              <div className="col-span-2">
                 <p>
                   {shippingAddress.fullName} <br />
                   {shippingAddress.street} <br />
@@ -182,22 +135,22 @@ export default function OrderDetailsForm({
           </div>
 
           {/* payment method */}
-          <div className='border-y'>
-            <div className='grid md:grid-cols-3 my-3 pb-3'>
-              <div className='text-lg font-bold'>
+          <div className="border-y">
+            <div className="grid md:grid-cols-3 my-3 pb-3">
+              <div className="text-lg font-bold">
                 <span>Payment Method</span>
               </div>
-              <div className='col-span-2'>
+              <div className="col-span-2">
                 <p>{paymentMethod}</p>
               </div>
             </div>
           </div>
 
-          <div className='grid md:grid-cols-3 my-3 pb-3'>
-            <div className='flex text-lg font-bold'>
+          <div className="grid md:grid-cols-3 my-3 pb-3">
+            <div className="flex text-lg font-bold">
               <span>Items and shipping</span>
             </div>
-            <div className='col-span-2'>
+            <div className="col-span-2">
               <p>
                 Delivery date:
                 {formatDateTime(expectedDeliveryDate).dateOnly}
@@ -211,16 +164,16 @@ export default function OrderDetailsForm({
               </ul>
             </div>
           </div>
-          <div className='block md:hidden'>
+          <div className="block md:hidden">
             <CheckoutSummary />
           </div>
 
           <CheckoutFooter />
         </div>
-        <div className='hidden md:block'>
+        <div className="hidden md:block">
           <CheckoutSummary />
         </div>
       </div>
     </main>
-  )
+  );
 }
